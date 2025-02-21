9a00ea0aa7030b554d8ae5b3fecd7e52f4e7a737
Stefania Domnisoru <stefania.domnisoru@Ipsos.com>
2025-02-21 15:39:33 +0200

    changes v1

--- src/test/java/ShareDataBrowser/Browser/ChromeService.java
+++ src/test/java/ShareDataBrowser/Browser/ChromeService.java
@@ -16,10 +16,15 @@ public class ChromeService implements BrowserServiceInterface{
 
     @Override
     public Object browserOptions() {
+        String ci_cd = System.getProperty("ci_cd");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--window-size=1920,1080");
+
+        if(Boolean.parseBoolean(ci_cd)) options.addArguments("--headless");
+
         return options;
     }
 
