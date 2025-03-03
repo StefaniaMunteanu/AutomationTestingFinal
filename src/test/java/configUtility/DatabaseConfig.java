2037ad9d8121e1521c2fab350f706b3c62eff814
Stefania <stefania.ioana.munteanu@gmail.com>
2025-03-04 00:04:23 +0200

    remove test with database connection 3

--- src/test/java/configUtility/DatabaseConfig.java
+++ src/test/java/configUtility/DatabaseConfig.java
@@ -1,5 +1,10 @@
 package configUtility;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DatabaseConfig {
 
     private String port;
@@ -7,35 +12,4 @@ public class DatabaseConfig {
     private String username;
     private String password;
 
-    public String getPort() {
-        return port;
-    }
-
-    public String getDatabase() {
-        return database;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public void setDatabase(String database) {
-        this.database = database;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
